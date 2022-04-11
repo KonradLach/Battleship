ba3e66e1e0d3e2323fc3e32efcea8195718e3551
Konrad Lach <Konrad@Konrads-MacBook-Air.local>
2022-04-10 21:14:30 -0700

    functioning place and start game method in gameBoardFactory. Working on recieveAttack method

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import {createContainer} from './domManip.js';
 
 createContainer();
 
+//Factory function to create the ships
 const shipFactory = (name,length) => {
+    //creates an array the size of the ships length to keep track of hits.
     let shipHealth = Array.from({length: `${length}`});
+
+    //updates the shipHealth array with the position the ship got hit in
     const hit = (index) => {
         if (index < length){
             shipHealth[index] = 'x'
             return shipHealth;
         };
     }
+
+    //if every value in the shipHealth array is hit this function returns that the ship is sunk
     const isSunk = () => {
         console.log(shipHealth);
         return shipHealth.every(val=> val === shipHealth[0]);
@@ -18,17 +24,71 @@ const shipFactory = (name,length) => {
     return {name,length,hit, isSunk}
 }
 
+//Factory function to create game board
 const gameBoardFactory = () =>{
-    let gameBoardSize = 10;
     let gameArray = [];
+    //create empty array to initialize the game
     const startGame = () =>{
-        for(let i=0; i<(gameBoardSize * gameBoardSize);i++){
-            gameArray.push('');
-        }    
+        gameArray = [
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','','']
+        ]
     }
 
-    return gameBoard
+    //places ship in row and column and orients how the player wants 
+    const place = (ship,row, column, orient) => {
+        //checks if where the ship is to be placed is empty
+        const canPlace = () => {
+            let empty = true
+            //checking if ship is already in the spot new ship is to be placed
+            if(orient === 'horizontal'){
+                for(let i = column; i< (column+ship.length); i++){
+                    if(gameArray[row][i] !== ''){
+                        empty = false
+                    }
+                }
+            }  
+            else if(orient === 'vertical'){
+                for(let i = row; i< (row+ship.length); i++){
+                    if(gameArray[i][column] !== ''){
+                        empty = false
+                    }
+                }
+            }  
+            return empty
+        };
+
+        //horizontal orrientation placement
+        if(orient === 'horizontal'  && canPlace()) {
+            for(let i = column; i< (column+ship.length); i++){
+                gameArray[row][i] = ship.name;
+            }
+        }
+
+        //vertical orrientations placement
+        else if(orient === 'vertical' && canPlace()) {
+            for(let i = row; i<((ship.length + row));i++){
+                gameArray[i][column] = ship.name;
+            }
+        }
+        return gameArray;
+    }
+
+    const recieveAttack = (row,column) =>{
+        if(gameArray[row][column] === ''){
+            gameArray[row][column] = 'x'
+        }
+        return gameArray
+    }
+    return{startGame, place, recieveAttack}
 }
-console.log("Hello");
-const sub = shipFactory('Submarine',3);
-export {shipFactory};
+
+export {shipFactory,gameBoardFactory};
--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,101 @@
-import {shipFactory} from "./index.js";
+import {shipFactory, gameBoardFactory} from "./index.js";
+describe('Ship Factory tests', () => {
 
-test('Ship factory produces ship', () => {
-    expect(shipFactory("Submarine",3).name).toEqual('Submarine');
-});
-test('Ship hit works', () => {
-    expect(shipFactory("Submarine",3).hit(1)).toEqual([undefined,'x',undefined])
-});
-describe('Is sunk working', () => {
-    let sub = shipFactory("Submarine",3);
-    sub.hit(0);
-    sub.hit(1);
-    sub.hit(2);
+    test('Ship factory produces ship', () => {
+        expect(shipFactory("Submarine",3).name).toEqual('Submarine');
+    });
+    test('Ship hit works', () => {
+        expect(shipFactory("Submarine",3).hit(1)).toEqual([undefined,'x',undefined])
+    });
     test('Ship sunk works',() =>{
+        let sub = shipFactory("Submarine",3);
+        sub.hit(0);
+        sub.hit(1);
+        sub.hit(2);
         expect(sub.isSunk()).toEqual(true);
     })
 
-})
+});
+
+describe('Gameboard Factory Tests', () => {
+    test('Gameboard places ships in location Horizontal',() =>{
+        const board = gameBoardFactory();
+        board.startGame();
+        expect(board.place(shipFactory('Submarine', 3),0,1,'horizontal')).toEqual([
+            ['','Submarine','Submarine','Submarine','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','','']])
+    })
+
+    test('Gameboard places ships in location Vertical',() =>{
+        const board = gameBoardFactory();
+        board.startGame();
+        expect(board.place(shipFactory('Submarine', 3),0,1,'vertical')).toEqual([
+            ['','Submarine','','','','','','','',''],
+            ['','Submarine','','','','','','','',''],
+            ['','Submarine','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','','']])
+    })
+    test('Gameboard doesn\'t intersect other ships',() =>{
+        const board = gameBoardFactory();
+        board.startGame();
+        board.place(shipFactory('Submarine', 3),0,1,'horizontal')
+        expect(board.place(shipFactory('Submarine', 3),0,1,'horizontal')).toEqual([
+            ['','Submarine','Submarine','Submarine','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','','']]);
+    })
+    test('Gameboard doesn\'t overflow on horizontal',() =>{
+        const board = gameBoardFactory();
+        board.startGame();
+        expect(board.place(shipFactory('Submarine', 3),0,9,'horizontal')).toEqual([
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','','']])
+    })
+
+    test('works and logs hit on empty map', () =>{
+        const board = gameBoardFactory();
+        board.startGame();
+        expect(board.recieveAttack(1,1)).toEqual([
+            ['','','','','','','','','',''],
+            ['','x','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','',''],
+            ['','','','','','','','','','']])
+    })
+});
+
+
