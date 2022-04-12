117c63377c99f2b107be8abdfec0a0cd917b104a
Konrad Lach <Konrad@Konrads-MacBook-Air.local>
2022-04-12 15:29:37 -0700

    Wrote gameboard factory and now workign on Player and computer player. Can't seem to access the game array.

--- src/index.js
+++ src/index.js
@@ -6,20 +6,19 @@ createContainer();
 //Factory function to create the ships
 const shipFactory = (name,length) => {
     //creates an array the size of the ships length to keep track of hits.
-    let shipHealth = Array.from({length: `${length}`});
+    const shipHealth = [];
 
     //updates the shipHealth array with the position the ship got hit in
-    const hit = (index) => {
-        if (index < length){
-            shipHealth[index] = 'x'
+    const hit = () => {
+        shipHealth.push('x')
             return shipHealth;
-        };
     }
-
     //if every value in the shipHealth array is hit this function returns that the ship is sunk
     const isSunk = () => {
-        console.log(shipHealth);
-        return shipHealth.every(val=> val === shipHealth[0]);
+        if(shipHealth.length === length){
+            return true
+        }
+        return false
     }
     return {name,length,hit, isSunk}
 }
@@ -29,7 +28,7 @@ const gameBoardFactory = () =>{
     let gameArray = [];
     //create empty array to initialize the game
     const startGame = () =>{
-        gameArray = [
+            gameArray = [
             ['','','','','','','','','',''],
             ['','','','','','','','','',''],
             ['','','','','','','','','',''],
@@ -41,7 +40,9 @@ const gameBoardFactory = () =>{
             ['','','','','','','','','',''],
             ['','','','','','','','','','']
         ]
+
     }
+    let shipsAlive = [];
 
     //places ship in row and column and orients how the player wants 
     const place = (ship,row, column, orient) => {
@@ -65,30 +66,73 @@ const gameBoardFactory = () =>{
             }  
             return empty
         };
-
         //horizontal orrientation placement
         if(orient === 'horizontal'  && canPlace()) {
             for(let i = column; i< (column+ship.length); i++){
-                gameArray[row][i] = ship.name;
+                gameArray[row][i] = ship;
             }
+            shipsAlive.push(ship.name)
         }
 
         //vertical orrientations placement
         else if(orient === 'vertical' && canPlace()) {
             for(let i = row; i<((ship.length + row));i++){
-                gameArray[i][column] = ship.name;
+                gameArray[i][column] = ship;
             }
         }
         return gameArray;
     }
 
+
     const recieveAttack = (row,column) =>{
         if(gameArray[row][column] === ''){
             gameArray[row][column] = 'x'
         }
+        else{
+            gameArray[row][column].hit();
+            if(gameArray[row][column].isSunk()){
+                shipsAlive.pop(gameArray[row][column].name)
+            }
+            gameArray[row][column] = 'X';
+            
+        }
         return gameArray
     }
-    return{startGame, place, recieveAttack}
+    return{startGame, place, recieveAttack,gameArray}
 }
 
-export {shipFactory,gameBoardFactory};
+const player = () =>{
+    let playerBoard = gameBoardFactory();
+    playerBoard.startGame();
+    const playerPlace = (ship,row,column,orient) =>{
+        playerBoard.place(ship,row,column,orient);
+        return playerBoard
+    }
+    return {playerBoard,playerPlace}
+}
+
+const computer = () => {
+    let computerBoard = gameBoardFactory();
+    computerBoard.startGame();
+    const computerRandomAttack = (playerBoard) =>{
+        let randomChoice = () => {
+            let randomRow = Math.floor(Math.random() *10)
+            let randomColumn = Math.floor(Math.random() * 10)
+            return [randomRow, randomColumn]
+        }
+        let random= randomChoice();
+        let row = random[0];
+        let column = random[1];
+        if(playerBoard.gameArray[row][column] === ''){
+            playerBoard.recieveAttack(random);
+            return playerBoard;
+        }
+        else{
+            computerRandomAttack(playerBoard)
+        }
+
+    }
+    return {computerBoard,computerRandomAttack}
+}
+
+export {shipFactory,gameBoardFactory, player,computer};
--- src/index.test.js
+++ src/index.test.js
@@ -1,70 +1,24 @@
-import {shipFactory, gameBoardFactory} from "./index.js";
+import {shipFactory, gameBoardFactory,player, computer} from "./index.js";
 describe('Ship Factory tests', () => {
 
     test('Ship factory produces ship', () => {
         expect(shipFactory("Submarine",3).name).toEqual('Submarine');
     });
     test('Ship hit works', () => {
-        expect(shipFactory("Submarine",3).hit(1)).toEqual([undefined,'x',undefined])
+        expect(shipFactory("Submarine",3).hit()).toEqual(['x'])
     });
     test('Ship sunk works',() =>{
         let sub = shipFactory("Submarine",3);
-        sub.hit(0);
-        sub.hit(1);
-        sub.hit(2);
+        sub.hit();
+        sub.hit();
+        sub.hit();
         expect(sub.isSunk()).toEqual(true);
     })
 
 });
 
 describe('Gameboard Factory Tests', () => {
-    test('Gameboard places ships in location Horizontal',() =>{
-        const board = gameBoardFactory();
-        board.startGame();
-        expect(board.place(shipFactory('Submarine', 3),0,1,'horizontal')).toEqual([
-            ['','Submarine','Submarine','Submarine','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','','']])
-    })
 
-    test('Gameboard places ships in location Vertical',() =>{
-        const board = gameBoardFactory();
-        board.startGame();
-        expect(board.place(shipFactory('Submarine', 3),0,1,'vertical')).toEqual([
-            ['','Submarine','','','','','','','',''],
-            ['','Submarine','','','','','','','',''],
-            ['','Submarine','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','','']])
-    })
-    test('Gameboard doesn\'t intersect other ships',() =>{
-        const board = gameBoardFactory();
-        board.startGame();
-        board.place(shipFactory('Submarine', 3),0,1,'horizontal')
-        expect(board.place(shipFactory('Submarine', 3),0,1,'horizontal')).toEqual([
-            ['','Submarine','Submarine','Submarine','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','',''],
-            ['','','','','','','','','','']]);
-    })
     test('Gameboard doesn\'t overflow on horizontal',() =>{
         const board = gameBoardFactory();
         board.startGame();
@@ -81,12 +35,13 @@ describe('Gameboard Factory Tests', () => {
             ['','','','','','','','','','']])
     })
 
-    test('works and logs hit on empty map', () =>{
+    test('RecieveAttack works and logs hit on empty map', () =>{
         const board = gameBoardFactory();
         board.startGame();
-        expect(board.recieveAttack(1,1)).toEqual([
+        board.recieveAttack(1,1)
+        expect(board.recieveAttack(1,2)).toEqual([
             ['','','','','','','','','',''],
-            ['','x','','','','','','','',''],
+            ['','x','x','','','','','','',''],
             ['','','','','','','','','',''],
             ['','','','','','','','','',''],
             ['','','','','','','','','',''],
@@ -95,6 +50,45 @@ describe('Gameboard Factory Tests', () => {
             ['','','','','','','','','',''],
             ['','','','','','','','','',''],
             ['','','','','','','','','','']])
+    })
+    test('Ship is sunk after being destroyed by recieve attack', () => {
+        const board = gameBoardFactory();
+        board.startGame();
+        const sub = shipFactory('Submarine',3);
+        board.place(sub,0,0,'horizontal');
+        board.recieveAttack(0,0);
+        board.recieveAttack(0,1);
+        board.recieveAttack(0,2);
+        expect(sub.isSunk()).toEqual(true);
+    })
+
+});
+
+describe('Player tests', () => {
+
+    test('Player can place all 5 ships',() =>{
+        const sub = shipFactory("submarine",3);
+        const carrier = shipFactory("carrier",5);
+        const battleship = shipFactory('battleship',4);
+        const cruiser = shipFactory('cruiser',3);
+        const destroyer = shipFactory('destroyer', 2);
+        const player1 = player();
+        player1.playerPlace(sub,0,0,"horizontal");
+        player1.playerPlace(carrier,2,0,'horizontal');
+        player1.playerPlace(battleship,4,0,'horizontal');
+        player1.playerPlace(cruiser,6,0,'horizontal');
+        player1.playerPlace(destroyer,8,0,'vertical');
+        player1.playerBoard.recieveAttack(0,1);
+        player1.playerBoard.recieveAttack(0,0);
+        player1.playerBoard.recieveAttack(0,2);
+        expect(sub.isSunk()).toEqual(true);
+    })
+    test('Computer random selection works', ()=>{
+        const player1 = player();
+        const computer1 = computer();
+
+        expect(computer1.computerRandomAttack(player1.playerBoard)).toContainer('x');
+
     })
 });
 
